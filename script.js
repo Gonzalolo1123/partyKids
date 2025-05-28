4abfa75db25a2a0130f260ad86d12a314ec39350
Gonzalo <gonzalolorenoz.garces@alumnos.ulagos.cl>
2025-05-27 22:41:53 -0400

    Primer commit del portafolio PartyKids

---
+++ script.js
@@ -0,0 +1,5 @@
+// Puedes agregar validaciones o interacciones aquí si lo necesitas
+document.querySelector('form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('¡Mensaje enviado!');
+});
